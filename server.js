254a0cda3fddee02ab7e9c60e3e84b82d7e28573
Yana <averyanusha@gmail.com>
2026-01-15 16:06:11 +0000

    Changed cors middleware

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
   origin: ['https://gseurodiffusion.fr'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
-app.options('/.*/', cors());
+app.options(/.*/, cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(searchRoutes);
