1b1e2b43732520b788c2efbb7237c5f0f788fdd6
Yana <averyanusha@gmail.com>
2026-06-09 11:20:59 +0100

    Changed render port for form submitting

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.post('/api/submit-form', [
     }
     const transporter = nodemailer.createTransport({
       host: 'smtp.office365.com',
-      port: 465,
+      port: 587,
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
